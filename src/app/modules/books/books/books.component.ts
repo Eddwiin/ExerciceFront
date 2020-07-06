e091a3cc69c7150e7aeb88f8d6a2c4f00bf43299
Eddwiin <maguran.mahendrarajan@gmail.com>
2020-07-06 19:14:07 +0200

    Fix bug design autocomplete and card

--- src/app/modules/books/books/books.component.ts
+++ src/app/modules/books/books/books.component.ts
@@ -2,23 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { BookModel } from '@core/resources/book/book.model';
 import { BookService } from '@core/resources/book/book.service';
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
 })
 export class BooksComponent implements OnInit {
 
   books$: Observable<BookModel[]>;
+  booksCopy: BookModel[];
 
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
-    this.books$ = this.bookService.getBooks();
+    this.books$ = this.bookService.getBooks()
+                    .pipe(
+                      map((books: BookModel[]) => this.booksCopy = [...books] )
+                    );
   }
 
-  convertBooksForAuto = (books: BookModel[]) => books.map((book: BookModel) => ({ id: book.isbn, label: book.title }) );
+  convertBooksForAuto = (books: BookModel[]) => books.map((book: BookModel) =>
+             ({ id: book.isbn, label: book.title,  price: book.price, cover: book.cover, synopsis: book.synopsis }) )
 
-  itemsChangeEvent(items) {}
+  inputChangedEvent = (books: BookModel[]) => this.booksCopy = [...books];
+
+  suggestionSelectedEvent = (book: BookModel) => this.booksCopy = [book];
 }
